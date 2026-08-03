f4b6c0e470cea99b5bdf55b5918fff194b02c203
git@github.com:powerwayhouse1008/GAI-Events-Hub.git <info@powerway.house>
2026-08-03 18:12:27 +0900

    Update project

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+    outputFileTracingIncludes: {
+      "*": [
+        "./node_modules/@sparticuz/chromium/bin/**/*",
+        "./node_modules/@sparticuz/chromium/build/**/*",
+      ],
+    },
   },
 };
 
